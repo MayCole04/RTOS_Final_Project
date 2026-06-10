31647512a59db3bfb1b0973713a39a48a675aa21
maly1077 <holb6018@kettering.edu>
2026-06-10 10:52:06 -0400

    Added new test task to test 7 segment

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 
 
-
+/*
 void test_task ( void * pvParameters ){
   gpio_pad_select_gpio(23); 
   gpio_set_direction(GPIO_NUM_23, GPIO_MODE_OUTPUT);
@@ -18,6 +18,30 @@ void test_task ( void * pvParameters ){
     //printf("Low\n");
   }
 }
+  */
+
+void test_task(void * pvParameters){
+  gpio_pad_select_gpio(18); 
+  gpio_set_direction(GPIO_NUM_18, GPIO_MODE_OUTPUT);
+  gpio_output_enable(GPIO_NUM_18);
+  gpio_set_level(GPIO_NUM_18, 1);
+
+  gpio_pad_select_gpio(17); 
+  gpio_set_direction(GPIO_NUM_17, GPIO_MODE_OUTPUT);
+  gpio_output_enable(GPIO_NUM_17);
+  gpio_set_level(GPIO_NUM_17, 1);
+  for(;;){
+    vTaskDelay(pdMS_TO_TICKS(500));
+    gpio_set_level(GPIO_NUM_18, 0);
+  //  vTaskDelay(pdMS_TO_TICKS(500));
+   // gpio_set_direction(GPIO_NUM_18, GPIO_MODE_OUTPUT);
+   // gpio_output_enable(GPIO_NUM_18);
+    //gpio_set_level(GPIO_NUM_18, 1);
+
+  }
+
+
+}
 
 
 
@@ -28,12 +52,12 @@ extern "C" {
       printf("sample queue is NULL\n");
     }
 
-    configPins();
+    //configPins();
     configTimer();
     xTaskCreatePinnedToCore(test_task,"test", 2000, NULL, 2, NULL, 0);
-    xTaskCreatePinnedToCore(beatMonitor_task, "monitor", 2000, NULL, 5, &beatMonitor_TaskHandle, 1 );
-    xTaskCreatePinnedToCore(userInput_task, "userInput", 2000, NULL, 5, NULL, 1 );
-    xTaskCreatePinnedToCore(LED_task,"LED", 2000, NULL, 2, &LED_TaskHandle, 1);
+    //xTaskCreatePinnedToCore(beatMonitor_task, "monitor", 2000, NULL, 5, &beatMonitor_TaskHandle, 1 );
+   //xTaskCreatePinnedToCore(userInput_task, "userInput", 2000, NULL, 5, NULL, 1 );
+    //xTaskCreatePinnedToCore(LED_task,"LED", 2000, NULL, 2, &LED_TaskHandle, 1);
 
     while (1) {
         // Yield to let other FreeRTOS tasks run
